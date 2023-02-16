d6af00d4516f3b5a32558a39c34b8fb21b64269d
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-16 13:02:53 -0800

    another correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {	/**
-	 * main - "programming is like buikding a multilingual puzzle
+	 * main - prints "programming is like buikding a multilingual
+	 *  puzzle, followed by a new line.
 	 * Returns:Always 0.
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
