25e3db752b63cce051b4578b583f39edcb480989
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-17 11:57:08 -0800

    do 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints three alphabets.
+ * Return: Returns 0.
+ */
+int main(void)
+{	char x = 'g';
+
+	char y = 'G';
+
+	putchar(x);
+	putchar(y);
+	return (0);
+}
+
