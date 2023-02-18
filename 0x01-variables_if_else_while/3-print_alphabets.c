dcfcd72e09f809a2bbbe5c5da9935887b071af73
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-18 03:08:38 -0800

    another correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - prints three alphabets.
- * Return: Returns 0.
-*/
+ * main - prints alphabets
+ * Return:returns 0
+ */
 int main(void)
 {	char x = 'a';
 
 	char y = 'A';
 
 	while (x <= 'z')
-	{putchar(x);
-	 x++;	
+	{	putchar(x);
+		x++;
 	}
 	while (y <= 'Z')
-	{putchar(y); 
-	 y++;
+	{	putchar(y);
+		y++;
 	}
-	return (0);	
+	putchar('\n');
+	return (0);
 }
