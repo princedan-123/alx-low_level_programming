92a7161e75fdbb7a8c77a746125d04b4b59fec40
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-18 01:45:14 -0800

    do 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print lower alphabets
+ * Return:returns 0
+ */
+int main(void)
+{	char x = 'a';
+
+	while (x <= 'z')
+	{	putchar(x);
+		x++;
+
+	}
+	return (0);
+}
