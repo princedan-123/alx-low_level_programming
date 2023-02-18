36cf2d799526040674ed7f0372a74c4ef862b40a
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-18 04:22:51 -0800

    do 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - prints alphabets execept q and e
+ * Return:returns 0.
+ */
+int main(void)
+{	char a = 'a';
+
+	char e = 'e';
+
+	char q = 'q';
+
+	char z = 'z';
+
+	while (a <= z)
+	{
+		if (a != e && q)
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
