d0ee31235c8f392c93484f878f39533c6e6e41b2
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-17 20:54:04 -0800

    do 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 characters
+ * Returns:it returns 0
+*/
+int main(void)
+{	int number = 1;
+
+	while (number < 10)
+	{printf("%d",number);
+	 number++;		
+	}	
+	return (0);
+}	
