398255086f9a9719a506ee1768a7a05a4479f8ba
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-21 09:16:59 -0800

    do second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return:Returns 0
+ */
+int main(void)
+{	char c = 'a'
+
+	while (c <= 'z')
+		{	_putchar(c);
+			c++;
+		}
+	_putchar('\n');
+	return (0);
+}
