7f9f0ae40ad5d71085cb16871d9552ed124c230c
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-21 08:13:14 -0800

    another correction

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,1 @@
-int _putchar(char '_');
-int _putchar(char 'p');
-int _putchar(char 'u');
-int _putchar(char 't');
-int _putchar(char 'c');
-int _putchar(char 'h');
-int _putchar(char 'a');
-int _putchar(char 'r');
-int _putchar(char '\n');
-
+int _putchar(char c);
