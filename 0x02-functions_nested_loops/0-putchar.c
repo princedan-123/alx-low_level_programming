0ab3cdb80a85a32472ff23c32d3c5fa3c3a49167
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-21 06:58:54 -0800

    do project 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<main.h>
+/**
+ * main -entry point
+ * Return: it will return 0
+ */
+int main(void)
+{       _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+}
