7d716779edfaa0729a81c2f4fce775117f54dd7a
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-21 12:26:26 -0800

    another correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,16 @@
  * maini - entry point
  * Return: it returns 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {	char c;
 
 	int i = 0;
 
-	while (i <9)
-		{	for (c ='a'; c <= 'z'; c++)
-			{	_putchar(c);
-
-			}
+	while (i <= 9)
+		{	for (c = 'a'; c <= 'z'; c++)
+				{	_putchar(c);
+				}
 			_putchar('\n');
-			i++;
+			i++;	
 		}
 }
