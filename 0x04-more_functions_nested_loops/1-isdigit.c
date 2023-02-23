8b36a82e19bc472975b318cdf7c71fad50db95ad
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-23 05:16:48 -0800

    do second task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isdigit - it checks for a number
+ * @c: the parameter that is checked
+ * Return: it returns 0 or 1
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) || (c <= 9))
+		return (1);
+	else
+		return (0);
+}
