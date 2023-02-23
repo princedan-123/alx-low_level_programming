df6b8930f19f9a71d0b0863fa92a64ffa9261295
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-23 04:13:34 -0800

    correction

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper(int c)- function that checkd case
+ * @c: function parameter
+ * Return: Returns 1 if c is uppercase else returns 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
