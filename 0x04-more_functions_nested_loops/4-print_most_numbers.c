b3133299053c1b9522dc746986f10c55d5bd4c19
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-23 06:51:35 -0800

    do fifth task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print from 0 - 9 except 2 and 4
+ * Return: Return 0
+ */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{ continue;
+		}
+	}	else
+		_putchar(x + '0');
+	_putchar('\n');
+}
