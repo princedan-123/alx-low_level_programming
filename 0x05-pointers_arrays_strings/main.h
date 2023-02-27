cecb32957a84e935ca4c0d369fae76168ff9fd02
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-27 05:30:09 -0800

    another correction

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,3 +12,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
