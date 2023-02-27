c88fc28ca83b396c661e5091564845ff4df605ba
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-27 06:34:10 -0800

    do 4-print-rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	for (c -= 1; c >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n');
+}
