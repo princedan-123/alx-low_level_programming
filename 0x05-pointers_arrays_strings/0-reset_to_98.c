e569aa22bfd89c89a3d629db79e26d522f575990
Daniel Mabia <danielebukamabia@gmail.com>
2023-02-27 05:16:19 -0800

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: the integer to be updated
+ * Return: it should return return n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (n);
+}
