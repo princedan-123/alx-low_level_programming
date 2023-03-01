5e87503b3a08aa2cd75cbe4745686b32fd86791d
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-01 04:36:03 -0800

    do task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper -  changes lower case letters
+ * @str:the string to be changed
+ * Return: a pointer  to the changed string
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= '2')
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
