cb9d1fa5308007f243204bbf8a9cce32a19f0def
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-01 02:53:15 -0800

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,10 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0;
 
-	for(i = 0; dest[i] != '\0'; i++);
-	for(j = 0; src[j] != '\0'; j++)
+	int j;
+
+	while (dest[i])
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
