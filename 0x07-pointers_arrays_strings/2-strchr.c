a3fbc1090b5413e01a896306941220aa7b695356
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-06 04:25:46 -0800

    do task two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - searches for a character in a string.
+ * @s: the string to be searched
+ * @c: the character to be searched for
+ * Return: returns a pointer or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (s);
+		}
+		else
+			return (NULL);
+	}
+}
