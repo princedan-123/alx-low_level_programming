2c4ebecf2256b1fa91f5d4be9224e397b149e250
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 04:40:37 -0800

    do task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - evaluates the factorial of a parameter
+ * @n: the parameter to be evaluated
+ * Return: integer values
+ */
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n-1));
+}
