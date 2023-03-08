fa3fe13ca962afa77c33f1c4c30400b7bf88b182
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 03:33:57 -0800

    do task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ * _print_rev_recursion - reverses a string
+ * @s: the string to be reversed
+ * Return: returns nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	s = '\0' - 1;
+	_putchar(*s);
+	--s;
+	_print_rev_recursion(*s);
+}
