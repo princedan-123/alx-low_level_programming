5a893a9c8b64c2526c52d5a4066d2eafed442a84
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 06:58:53 -0800

    do task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number - evaluates in a number is a prime number
+ * @n: the number to be evaluated
+ * @i: the interation number
+ */
+int is_prime_number(int n, int i)
+{
+	if (n < 1)
+		return (0);
+	if ((n != i) && (n % i = 0))
+		return (0);
+	return (is_prime_number(n, i+1));	
+}
