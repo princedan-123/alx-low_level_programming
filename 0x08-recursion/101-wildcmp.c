31d52297e20cdc7ebfbd29622e8df1947aac9f22
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 08:28:57 -0800

    another correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,12 +7,12 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (i != '\0')
+	if ((*s1 != '\0') && (*s2 != 0))
 	{
 		if (*s1 == *s2)
 			return (1);
 	}
-	if (i == '\0')
+	if (*s1 == '\0')
 		return (0);
 	return (s1++, s2++, wildcmp(s1, s2,);
 }
