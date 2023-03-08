aae4c0e4c504cdb4ffdc3c804c3f62b182a6fbcd
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 06:11:14 -0800

    do task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,14 +3,15 @@
  * _sqrt_recursion - calculates the natural square root of n
  * 
  * @n: the number to be calculated
- * @i: the iteration number
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n,)
 {
+	int i;
+
 	i = 1;
 	if (i > n)
 		return (-1);
 	if (i == n)
 		return (i);
-	return (_sqrt_recursion(n, i++))
+	return (_sqrt_recursion(n, i++));
 }
