4ae1a4a1d7dbb77e8b1afd3bf06edddb8b0e641d
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 05:33:43 -0800

    do task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - computes the value of an integer
+ * @x: the first parameter
+ * @y: the power
+ * Return: integers
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x,y--);
+}
