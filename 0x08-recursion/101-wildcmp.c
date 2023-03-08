5f3469f92bb51d295e36794ce037df30e44615e7
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-08 08:19:44 -0800

    do task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,17 +3,16 @@
  * wildcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * @i: iteration number
  * Return: 1 or 0
  */
-int wildcmp(char *s1, char *s2, int i)
+int wildcmp(char *s1, char *s2,)
 {
 	if (i != '\0')
 	{
-		if (*(s1[i]) == (*s2[i]))
+		if (*s1 == *s2)
 			return (1);
 	}
 	if (i == '\0')
 		return (0);
-	wildcmp(s1, s2, i + 1);
+	return (s1++, s2++, wildcmp(s1, s2,);
 }
