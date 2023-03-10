277695cf5114d9f497ecdb69cac3eafe883d2200
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-10 08:53:52 -0800

    do task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments it recieved
+ * @argc: the parameter that stores argument count
+ * @argv: the parameter that stores argument string
+ * Return: it returns zero
+ */
+int main(int argc, char *argv)
+{
+	(void) argv[];
+	printf("\n %d", argc);
+	return (0);
+}
