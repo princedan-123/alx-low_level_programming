adfc97f776c15a7b7694a2d19bdefa3b0646fec5
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-10 08:43:45 -0800

    another correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,12 @@
 /**
  * main - prints the name of the program
  * @argv: the parameter of the function
+ * @argc: parameter of the function
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	(void) argc;
 	return (0);
 }
