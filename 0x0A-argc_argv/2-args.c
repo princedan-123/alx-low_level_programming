23852d2a45822796523d33d6acab6bd8c9622ed8
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-10 10:11:17 -0800

    another correction

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,7 +9,8 @@
 int main(int argc, char *argv[])
 {	int i;
 
-	for (i = 1; i <= argc; i++)
-		printf("/n %s", argv[i]);
+	for (i = 0; i < argc; i++)
+		printf("\n %s", argv[i]);
+	printf("\n");
 	return (0);
 }
