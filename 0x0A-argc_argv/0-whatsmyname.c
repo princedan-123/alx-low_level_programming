57551554268061e477d87aeffb2d6c8ab3a4b932
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-10 08:28:53 -0800

    do task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argv: the parameter of the function
+ * Return: 0
+ */
+int main(char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
