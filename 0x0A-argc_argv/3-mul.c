1a775c21afcccefd938aee3a748e4f90fc91ddbb
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-11 12:46:04 -0800

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -42,7 +42,11 @@ int _atoi(char *s)
  * @argv: the strings
  */
 int main(int argc, char *argv[])
-{	int num1; int num2; int result;
+{	int num1;
+
+	int num2;
+
+	int result;
 
 	if ((argc > 3) || (argc < 3))
 	{
@@ -51,11 +55,11 @@ int main(int argc, char *argv[])
 	}
 
 	 num1 = _atoi(argv[1]);
-	
+
 	 num2 = _atoi(argv[2]);
 
 	 result = num1 * num2;
-	
+
 	printf("%d\n", result);
 	return (0);
 }
