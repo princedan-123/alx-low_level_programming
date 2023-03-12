67bc5dc33f5ccfe85f402b9056decd9acdb87cee
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-12 07:38:40 -0700

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -50,7 +50,10 @@ int main(int argc, char *argv[])
 	int result;
 
 	if (argc < 3)
-		printf("0");
+	{
+		printf("0\n");
+		return (1);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
