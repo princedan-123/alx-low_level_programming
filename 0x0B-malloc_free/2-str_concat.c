1f43e63788d19d670b13415da01150f88d9d2355
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-15 10:32:31 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,14 +11,17 @@ char *str_concat(char *s1, char *s2)
 
 	char *m;
 
-	size_t n1 = _strlen(s1);
+	size_t n1;
 
-	size_t n2 = _strlen(s2);
+	size_t n2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	n1 = _strlen(s1);
+
+	n2 = _strlen(s2);
 
 	m = malloc(n1 + n2 + 1);
 	if (m == NULL)
