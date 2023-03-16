a06d48bab762280c07fe8b106771d7cc6197a113
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-16 08:15:13 -0700

    correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,13 +14,15 @@ int *array_range(int min, int max)
 
 	int *array;
 
+	int size = max - min + 1;
+
 	if (min > max)
 		return (NULL);
-	array = malloc(sizeof(int) * max);
+	array = malloc(sizeof(int) * size);
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < max; i++)
+	for (i = 0; i < size; i++)
 		array[i] = min + i;
 	return (array);
 }
