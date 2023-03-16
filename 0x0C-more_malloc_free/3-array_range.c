24e439f5a7e81dadbda6ecaa017b7cf00a4c5e9e
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-16 06:58:20 -0700

    do task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - allocates memory for an array
+ * @min: minium value of the array element
+ * @max: maximum value of the array element
+ * Return: NULL if it fails or a pointer if successful
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(int) * max);
+	if (array == NULL)
+		return (NULL);
+	array[0] = min;
+
+	for (i = 0; i < max; i++)
+	{
+		if (array[0] == min)
+			continue;
+		array[i]=+ 1;
+	}
+	return (array);
+}
