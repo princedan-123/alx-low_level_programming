cb4e0bc02e5237df625687a82932afdfa07eab9d
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-17 02:48:20 -0700

    correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,6 +2,22 @@
 #include "main.h"
 
 /**
+ * _memcpy - copies the value of an address
+ * @dest:destination
+ * @src:source
+ * @n: size of the value to be copied
+ * Return: return the pointer dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+/**
  * _realloc - reallocates memory
  * @ptr: a pointer to the old memory
  * @old_size: intial size of the old memory
@@ -11,11 +27,11 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_memory;
 
-	ptr = malloc(old_size);
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-	if (new_size > old_size)
-		ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
 	if ((new_size == 0) && (ptr != NULL))
@@ -23,5 +39,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	return (ptr);
+	new_memory = malloc(new_size);
+	if (new_size > old_size)
+	{
+		_memcpy(new_memory, ptr, old_size);
+	}
+	free(ptr);
+	return (new_memory);
 }
