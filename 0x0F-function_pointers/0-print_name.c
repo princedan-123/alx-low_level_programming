e2e5fb22765916b9aade590539ea9aa970d665d4
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-22 05:03:46 -0700

    do task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_format - prints a name as is
+ * @name the name to be printed
+ * Return: nothing
+ */
+
+void print_format(char *name)
+{
+	printf("Hello, my name is %s\n", name);
+}
+/**
+ * print_name - prints a name
+ * @name: the name to  be printed
+ * @f: a function pointer
+ * Return: it returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
