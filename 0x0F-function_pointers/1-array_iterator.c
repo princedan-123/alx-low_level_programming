f6e1ce9bd2bf34940c42e9a220bf2a9785d110b2
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-22 06:24:20 -0700

    do task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_array - prints the elements of an array
+ * @index: the index of each elements
+ * Return: nothing is returned
+ */
+
+void print_array(int index)
+{
+	_putchar((char)index);
+}
+
+/**
+ * array_iterator - iterates over an array to print its elements
+ * @array: the pointer to the elements of the array
+ * @size: the size of the array
+ * @action: a function pointer to the index
+ * Return: nothing is returned
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
