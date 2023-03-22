fd0721055f368a32cac1f9dca208da7f11685981
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-22 07:32:43 -0700

    updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,3 +3,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 void print_format(char *name);
 int _putchar(char c);
+int compare(int name);
