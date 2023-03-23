3798930abd8cb9a2bc23cdf609092968b1818f4a
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-23 03:45:58 -0700

    do task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - A variadic function that prints out integers
+ * @seperator: A string that seperates the integers
+ * @n: The maximum number of integers to be printed
+ * Return: Nothing is returned
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	int x = 0;
+
+	va_list print;
+
+	va_start(print, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(print, int);
+
+		printf("%d", x);
+		printf("%s", separator);
+	}
+	va_end(print);
+	printf("\n");
+}
