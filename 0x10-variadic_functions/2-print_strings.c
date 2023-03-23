68994db4bee9e0fe3fbf4143c5a2a7d02aa1ba13
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-23 11:50:30 -0700

    correction to separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,8 +16,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(string, n);
 
-	if (separator == NULL)
-		return;
+	/*if (separator == NULL)*/
+		/*return;*/
 
 	for (i = 0; i < n; i++)
 	{
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 
