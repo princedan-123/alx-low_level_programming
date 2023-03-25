ab16cb7c48ed12555d1f548b5f0f226d9351a645
Daniel Mabia <danielebukamabia@gmail.com>
2023-03-25 04:09:25 -0700

    changed the pointer identifyer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	int (*cal)(int, int);
 
-	char *op;
+	char *s;
 
 	if (argc != 4)
 	{
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 
 	num2 = atoi(argv[3]);
 
-	op = argv[2];
+	s = argv[2];
 	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	cal = get_op_func(op);
+	cal = get_op_func(s);
 	if (cal == NULL)
 	{
 		printf("Error\n");
