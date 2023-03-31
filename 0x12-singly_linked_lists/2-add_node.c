004b79d72e8742fe8301360c7a82d60244f4576c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-03-31 03:01:26 -0700

    freed unused memory to avoid leaks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,14 +16,19 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 	/*NULL check*/
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 	/*copy the string into the node and set length*/
 	new->str = strdup(str);
 
 	new->len = strlen(str);
 	/*NULL Check*/
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	/*insert node*/
 	new->next = *head;
 	*head = new;
