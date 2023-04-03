22c53278788e6b39e7dab979f4d159658fb4b1b0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-03 09:00:57 -0700

    do task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint -sums up all the data in the nodes of a list
+ * @head: the head of the node
+ * Return: the sum is returned
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *tmp = head;
+
+	int sum = 0;
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		
+		tmp = tmp->next;
+	}
+	return(sum);
+}
