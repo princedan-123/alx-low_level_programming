1a6e093a4920a2e26302d54892100e242eb5a1b3
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-04 02:44:15 -0700

    removed null check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,8 +20,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_node == NULL)/*NULL Check*/
 		return (NULL);
-	if (*head == NULL)
-		return (NULL);
 	if (idx == 0)
 	{
 		*head = new_node;
