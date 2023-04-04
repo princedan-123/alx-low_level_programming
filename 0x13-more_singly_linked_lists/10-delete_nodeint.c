f71a117ba999129a50ba4fca92a10b68b307865a
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-04 05:57:39 -0700

    added condtition for index 0

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,9 +33,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		tmp = tmp->next;
 		*head = tmp->next;
 		free(tmp);
+		return (1);
 	}
 	while (count <= (index - 1))
 	{
