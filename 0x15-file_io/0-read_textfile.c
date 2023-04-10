fdaa5378dfa63d69dc20c33ed08f18ee6d80ed44
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-10 01:52:19 -0700

    removed the double quotes in the open function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("filename", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 	byteR = read(fd, buffer, letters);
