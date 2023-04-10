4a176cf79e3b8c3edbfae653f4c2bc7b4be020ce
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-10 05:43:01 -0700

    do task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file - appends  a string to a file
+ * @filename: name of the file
+ * @text_content: the string to be added to the file
+ * Return: 1 if successful or -1 if it fails
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int byteW, fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND);
+	if (fd < 0)
+		return (-1);
+	else if (text_content == NULL)
+		close(fd);
+		return (1);
+	if (text_content != NULL)
+		byteW = write(fd, text_content, strlen(text_content));
+	if (byteW < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
