5b75e577cafb6dc1a67381be2a29f3525ddfc304
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-11 01:48:29 -0700

    added condition for null string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	byteW = write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		byteW = write(fd, text_content, strlen(text_content));
 	if (byteW < 0)
 		return (-1);
 	close(fd);
