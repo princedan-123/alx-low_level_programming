db9fc2397d76b2ac9018a637777bcc6938ce59d8
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-11 01:57:34 -0700

    included write only permission

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,11 +15,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
-	else if (text_content == NULL)
-		return (1);
 	if (text_content != NULL)
 		byteW = write(fd, text_content, strlen(text_content));
 	if (byteW < 0)
