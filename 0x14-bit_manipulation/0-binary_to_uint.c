4096464afedde51ac7f8538b29a9715bc0b7e229
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-04-12 01:27:10 -0700

    edge case of null string fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - converts to decimal
@@ -18,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	unsigned int result = 0;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0); /*check for empty string*/
 	for (i = 0; b[i] != '\0'; i++)/*check for non-digit characters*/
 	{
