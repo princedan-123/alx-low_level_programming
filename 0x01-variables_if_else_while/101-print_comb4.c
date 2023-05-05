69c05c70cb20cbd878907fb8b4763d8088d6611c
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-05 09:31:07 -0700

    do task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the combination of three numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (x = j + 1; x < 10; x++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(x + '0');
+				if ((i == 7) && (j == 8) && (x = 9))
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
