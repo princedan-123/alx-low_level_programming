577afe46fa611d2dd4f6b766d96bc813b22956e7
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-08 04:22:18 -0700

    ensured header file was guarded

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef HEADER
 #define HEADER
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
