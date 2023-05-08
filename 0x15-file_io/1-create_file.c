f17e0ad01cbce08843aa71203be46044f8944e5a
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-08 04:45:33 -0700

    used strlen instead of sizeof operator

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - creates a file and writes into it
@@ -18,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	fdw = write(fd, text_content, sizeof(text_content));
+	fdw = write(fd, text_content, strlen(text_content));
 	if (fdw == -1)
 		return (-1);
 	close(fd);
