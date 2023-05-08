a5ed8df1acad4b6b4bc0389483b7a2e7e12a2357
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-08 04:48:18 -0700

    replaced sizeof operator with strlen

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * append_text_to_file - appends content to a file
@@ -18,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	fdw = write(fd, text_content, sizeof(text_content));
+	fdw = write(fd, text_content, strlen(text_content));
 	if (fdw == -1)
 		return (-1);
 	close(fd);
