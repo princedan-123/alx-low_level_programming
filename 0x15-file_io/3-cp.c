34dd2e0054a134187cb6d8148ded7c36f90cc4cf
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-08 08:33:09 -0700

    do task 4

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - copies the content of a file into another
+ * @argc: argument count
+ * @argv: an array of strings passed as arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, fdr, fdw, cfd1, cfd2;
+
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage:cp %s %s\n", argv[1], argv[2]);
+			exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		fdr = read(fd1, buffer, 1024);
+		if ((fd1 == -1) || (fdr == -1))
+		{
+			dprintf(STDERR_FILENO, "Error: can't read from %s\n", argv[1]);
+			exit(98);
+		}
+		fdw = write(fd2, buffer, fdr);
+		if ((fd2 == -1) || (fdw == -1))
+		{
+			dprintf(STDERR_FILENO, "Error: cant't write to %s\n", argv[2]);
+			exit(99);
+		}
+	} while (fdr != 0);
+	cfd1 = close(fd1);
+	cfd2 = close(fd2);
+	if (cfd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %i\n", fd1);
+		exit(100);
+	}
+	if (cfd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %i\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
