2a81fd5ca5ee18396a490e057849b0232d129afe
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-05-08 03:54:04 -0700

    task 2 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes into it
+ * @filename: the name of the file to be created
+ * @text_content: the content to be written into the file
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fdw;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	fdw = write(fd, text_content, sizeof(text_content));
+	if (fdw == -1)
+		return (-1);
+	return (1);
+}
