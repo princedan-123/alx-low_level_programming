ee8bfba328529a65d33d4207313c593cfffcf0c4
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 05:48:29 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums the integer data in a list
+ * @head: the head of the list
+ * Return: 0 if list is empty or the sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+	int sum = 0;
+	if (tmp == NULL)
+		return (sum);
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
