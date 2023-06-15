9b4bf74bcd454181fb5e72facadf2c1c2c700d52
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 10:16:42 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,28 +11,31 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *tmp = *h;
-	dlistint_t *tmp1 = NULL;
+	dlistint_t *tmp = *h, *node = NULL, *previous = NULL;
 	unsigned int count = 0;
 
 	if (new == NULL)
 		return (NULL);
 	if (idx == 0)
-		add_dnodeint(h, n);
-	while (*h != NULL)
 	{
-		*h = (*h)->next;
-		tmp1 = (*h)->prev;
-		tmp = (*h)->next;
+		node = add_dnodeint(h, n);
+		return (node);
+	}
+	while (tmp != NULL)
+	{
+		previous = *h;
+		tmp = tmp->next;
 		count++;
 		if (count == idx)
 		{
-			new->n = n;
-			new->prev = (*h)->prev;
-			new->next = *h;
-			(*h)->prev = new;
-			(*h)->next = tmp->next;
-			tmp1->next = new;
+			if (previous != NULL)
+			{
+				new->n = n;
+				new->prev = previous;
+				previous->next = new;
+				new->next = tmp;
+				tmp->prev = new;
+			}
 		}
 		return (new);
 	}
