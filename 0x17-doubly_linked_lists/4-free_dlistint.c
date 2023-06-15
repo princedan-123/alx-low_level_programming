cd241c3fefc03d32549d06d5c07dd9d90bcf2bb4
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 05:15:41 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a double linked list
+ * @head: the head of the list
+ * Return: nothing is returned
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = NULL;
+	if (head == NULL)
+		free(head);
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
