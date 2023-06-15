4229bc69d385f3873d120e12f420af6af35cdadd
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 04:32:22 -0700

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,6 +15,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL && tmp == NULL)
 		return (NULL);
 	tmp = *head;
+	new->n = n;
+	new->next = NULL;
 	if (*head == NULL)
 	{
 		new->prev = NULL;
@@ -29,8 +31,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
 	new->prev = tmp;
 	tmp->next = new;
 	return (new);
