136d50edb32ce89280f85ab4ae82685535940ff7
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 12:38:54 -0700

    updated header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,9 +11,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 /* standard header files*/
 #include <stdlib.h>
