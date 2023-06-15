8c7cddc2646a15ef75c2d66f72a8ebccbb763721
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-15 02:19:42 -0700

    task1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the number of elements in a node
+ * @h: the head pointer
+ * Return: the number of node is returned
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
