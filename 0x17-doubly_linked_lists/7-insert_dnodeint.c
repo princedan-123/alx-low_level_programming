ec96eeee6ac5f0de07a65880dbf78ff9af4d3f3e
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-06-16 01:44:54 -0700

    treated edge case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
-	if (idx == 0)
+	if (idx == 0 && tmp != NULL)
 	{
 		node = add_dnodeint(h, n);
 		return (node);
