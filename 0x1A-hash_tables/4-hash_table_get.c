f7281457810c39298ca0dd989997a87f51f651cb
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-07 05:52:52 -0700

    added edge cases

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current = NULL;
 
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 	if (current == NULL)
