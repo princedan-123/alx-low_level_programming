ddff49db983256b0775da5cf60acb2655eb4ee11
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-06 23:56:43 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
+		return (NULL);
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	table->array = malloc(sizeof(hash_node_t) * size);
-	if (table->array == NULL)
-	{
-		free(table->array);
-		return (NULL);
-	}
 	table->size = size;
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	} /*Making sure the array is empty*/
 	return (table);
 }
