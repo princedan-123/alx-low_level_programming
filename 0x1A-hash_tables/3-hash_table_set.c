dd46a892e9be788d9274c47cb46d057edfe9dc71
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-08 06:33:22 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *current = NULL, *item = NULL;
 	unsigned long int index = 0;
+	char *value_copy = NULL;
 
 	if ((key == NULL) || (strlen(key) == 0) || (ht == NULL))
 		return (0);
@@ -19,18 +20,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	item = malloc(sizeof(hash_node_t));
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 	item->key = strdup(key);
 	if (item->key == NULL)
 	{
 		free(item->key);
 		free(item);
 	}
-	item->value = strdup(value);
-	if (item->value == NULL)
-	{
-		free(item->value);
-		free(item);
-	}
+	item->value = value_copy;
 	item->next = NULL;
 	current = ht->array[index];
 	if (current == NULL)
@@ -40,12 +39,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current->value = strdup(value);
-			if (current->value == NULL)
-			{
-				free(current->value);
-				free(current);
-			}
+			free(item->key);
+			free(item->value);
+			free(item);
+			current->value = value_copy;
 			return (1);
 		}
 		current = current->next;
