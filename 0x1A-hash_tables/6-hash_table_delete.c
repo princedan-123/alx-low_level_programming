3a2bc2dd7c940bbca4067b5ac33931cda1ed86ee
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-08 11:22:10 -0700

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to the hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *current = NULL, *tmp = NULL;
+
+	if (ht == NULL)
+		exit(EXIT_SUCCESS);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array != NULL)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				tmp = current;
+				current = current->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+			}
+		}
+
+	}
+}
