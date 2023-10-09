e7d7388a2aa7d0d0dab6e0da22fb77ad8450b2eb
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-10-09 09:41:43 +0000

    Header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+/** function prototypes **/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
