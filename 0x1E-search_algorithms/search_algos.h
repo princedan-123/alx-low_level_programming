1b7f6767b50cfe66a455b2cfcc68cea729e293ee
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-10-10 08:10:48 +0000

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,4 +3,5 @@
 /** function prototypes **/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
 #endif
