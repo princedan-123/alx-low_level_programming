93e86940eaaef3254928f034f11693aefd2e640f
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-10-11 13:13:59 +0000

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int left_index, int right_index);
 int jump_search(int *array, size_t size, int value);
+int binary_search_algo(int *array, int low, int high, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
