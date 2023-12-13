fe06e47e3b00f8986c6d4ac5ea35fb88c895eedb
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-12-13 16:30:24 +0000

    updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int left, int right);
 int jump_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
